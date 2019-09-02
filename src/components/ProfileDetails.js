653faf9e0ac232426b1e71fd06c1d3dd1740cf72
arjun2010.ece <arjun2010.ece@gmail.com>
2019-09-02 01:18:16 +0100

    profile image not loading from the backend

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -22,8 +22,12 @@ class ProfileDetails extends Component {
           <p>{this.props.profile.lastName}</p>
         </div>
         <div className="prof-detail-img">
-          {/* <h4>Profile Pic :</h4> */}
-          <p><img src={`this.props.profile.productImage.split('/')[1]`} alt={this.props.profile.firstName}/></p>
+          {this.props.profile.productImage && (
+            <p><img src={`this.props.profile.productImage`} alt={this.props.profile.firstName}/></p>
+          )}
+          {!this.props.profile.productImage && (
+            <p><img src="/img/no_profile.png" alt="Image not available" /></p>
+          )}
         </div>
         <div className="prof-detail">
           <h4>Email :</h4>
