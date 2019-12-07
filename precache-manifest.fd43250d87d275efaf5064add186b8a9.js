c402cb6071a0c585f2b377eb2055ce23c98f7ad9
arjun2010-ece <arjun2010.ece@gmail.com>
2019-12-07 10:51:40 +0000

    Updates

---
+++ precache-manifest.fd43250d87d275efaf5064add186b8a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c950a06ff778ed21cc4c0cc0857349",
+    "url": "/Profile_crud_frontend/index.html"
+  },
+  {
+    "revision": "de0bc642df9477055f52",
+    "url": "/Profile_crud_frontend/static/css/main.4eb8cae8.chunk.css"
+  },
+  {
+    "revision": "2bd27d8a8e8324bda89b",
+    "url": "/Profile_crud_frontend/static/js/2.5b6947b7.chunk.js"
+  },
+  {
+    "revision": "de0bc642df9477055f52",
+    "url": "/Profile_crud_frontend/static/js/main.d144193f.chunk.js"
+  },
+  {
+    "revision": "b81df245e5199b5583ae",
+    "url": "/Profile_crud_frontend/static/js/runtime~main.1742c30b.js"
+  }
+]);
